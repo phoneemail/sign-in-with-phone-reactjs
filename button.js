5bd70ce888d440ed17b187f4b61a7f92187eac07
phoneemail <148524013+phoneemail@users.noreply.github.com>
2024-01-15 15:44:39 +0530

    Update button.js
    Update

--- button.js
+++ button.js
@@ -1,21 +1,19 @@
 import React from 'react';
 
-function YourComponent() {
-  const COUNTRY_CODE = 'your_country_code';
-  const PHONE_NO = 'your_phone_number';
-  const REDIRECT_URL = 'your_redirect_url';
+class YourComponent extends React.Component {
+  openPhoneLoginWindow = () => {
+    const countrycode = 'your_country_code';  // Replace with the actual country code
+    const phoneNo = 'your_phone_number';      // Replace with the actual phone number
+    const redirectUrl = 'your_redirect_url';  // Replace with the actual redirect URL
 
-  const openLoginWindow = () => {
-    window.open(
-      `https://auth.phone.email/sign-in?countrycode=${COUNTRY_CODE}&phone_no=${PHONE_NO}&redirect_url=${REDIRECT_URL}`,
-      'peLoginWindow',
-      'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0, width=500, height=500, top=' + (window.screen.height - 500) / 2 + ', left=' + (window.screen.width - 500) / 2
-    );
+    const url = `https://www.phone.email/auth/sign-in?countrycode=${countrycode}&phone_no=${phoneNo}&redirect_url=${redirectUrl}`;
+    
+    window.open(url, 'peLoginWindow', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=560,top=' + (screen.height - 600) / 2 + ',left=' + (screen.width - 500) / 2);
   };
 
-  return (
-    <button
-      style={{
+  render() {
+    return (
+      <button style={{
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'space-between',
@@ -28,23 +26,13 @@ function YourComponent() {
         fontSize: 'inherit',
         cursor: 'pointer',
       }}
-      id="btn_ph_login"
-      name="btn_ph_login"
-      type="button"
-      onClick={openLoginWindow}
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        style={{ marginRight: '5px', fill: '#ffffff' }}
-        height="24"
-        viewBox="0 -960 960 960"
-        width="24"
-      >
-        <path d="M798-120q-125 0-247-54.5T329-329Q229-429 174.5-551T120-798q0-18 12-30t30-12h162q14 0 25 9.5t13 22.5l26 140q2 16-1 27t-11 19l-97 98q20 37 47.5 71.5T387-386q31 31 65 57.5t72 48.5l94-94q9-9 23.5-13.5T670-390l138 28q14 4 23 14.5t9 23.5v162q0 18-12 30t-30 12Z" />
-      </svg>
-      Sign in with Phone Number
-    </button>
-  );
+      onClick={this.openPhoneLoginWindow}>
+        <svg xmlns="http://www.w3.org/2000/svg" style={{ marginRight: '5px', fill: '#ffffff' }} height="24" viewBox="0 -960 960 960" width="24">
+          <path d="M798-120q-125 0-247-54.5T329-329Q229-429 174.5-551T120-798q0-18 12-30t30-12h162q14 0 25 9.5t13 22.5l26 140q2 16-1 27t-11 19l-97 98q20 37 47.5 71.5T387-386q31 31 65 57.5t72 48.5l94-94q9-9 23.5-13.5T670-390l138 28q14 4 23 14.5t9 23.5v162q0 18-12 30t-30 12Z"/>
+        </svg> Sign in with Phone Number
+      </button>
+    );
+  }
 }
 
 export default YourComponent;
