915fe0b8b3a59fb21c01243e773bbac4e4a3160b
Devendra <dev@phone.email>
2024-02-23 13:42:55 +0530

    Single

--- src/Login.js
+++ src/Login.js
@@ -45,9 +45,20 @@ const App = () => {
         phEmailJwt: phEmailJwt
       });
 
-      // Set cookie with 1-day expiration
-      const cookieExpire = new Date(Date.now() + 1 * 24 * 60 * 60 * 1000).toUTCString();
+      // Set cookie with 180-day expiration
+      const cookieExpire = new Date(Date.now() + 180 * 24 * 60 * 60 * 1000).toUTCString();
       document.cookie = `ph_email_jwt=${phEmailJwt}; expires=${cookieExpire}; path=/`;
+
+      // Register User: As the user phone number has been verified successfully. If user corrosponding to this verified  mobile number does not exist in your user table then register the user by creating a row in user table. If user already exists then simply continue to the next step.
+
+      // Send Email: We reccomend you to send welcome email to the user.
+      //curl --location --request POST "https://api.phone.email/v1/sendmail" --ssl-no-revoke --header "Content-Type: application/json" --data-raw "{'apiKey':'API_KEY','fromCountryCode':'XXX','fromPhoneNo':'XXXXXXXXXX', 'toCountrycode':'XX','toPhoneNo':'XXXXXXXXXX','subject': 'Welcome to YOUR_BUSINESS_NAME','tinyFlag':true,'messageBody':'V2VsY29tZSB0byB5b3VyIEJVU0lORVNTX05BTUU='}"
+
+      // Create Session: Store verified user phone number in session variable.
+
+      // Redirect: Redirect user to the page of your choice as the user has successfully logged in.
+
+      // Handle Logout (Optional): You can create logout button on your website as required.In the event of logout you must clear delete ph_email_jwt cookie and clear your session variables.  To delete cookie simply set it to blank -> setcookie("ph_email_jwt", "", time()-3600);
 
     } catch (error) {
       console.error("Fetch error:", error);
@@ -63,14 +74,14 @@ const App = () => {
   return (
     <React.Fragment>
       {!accessToken && (
-        <div className="phem-container">
-          <div className="phem-card">
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', margin: '50px 30px' }}>
+          <div style={{ color: '#024430 !important', textAlign: 'center', backgroundColor: '#fff', padding: '30px', borderRadius: '0.5rem', boxShadow: '0 1px 3px rgba(17, 24, 39, .09)', width: '100%', maxWidth: '420px', margin: '0 auto', fontFamily: 'sans-serif, serif, system-ui, -apple-system, BlinkMacSystemFont, \'Segoe UI\', Roboto, Oxygen, Ubuntu, Cantarell, \'Open Sans\', \'Helvetica Neue\', sans-serif', lineHeight: '28px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
             <img className="phe-login-img" width="250px" src="https://storage.googleapis.com/prod-phoneemail-prof-images/phem-widgets/phe-signin-box.svg"
               alt="phone email login demo" />
             <h1 style={{ margin: "10px" }}>Sign In</h1>
             <p style={{ color: "#a6a6a6" }}>Welcome to Sign In with Phone</p>
             <button
-              className="phem-login-button"
+              style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '14px 20px', backgroundColor: '#02BD7E', fontWeight: 'bold', color: '#ffffff', border: 'none', borderRadius: '3px', fontSize: 'inherit', cursor: 'pointer', maxWidth: '320px', width: '100%' }}
               id="btn_ph_login"
               name="btn_ph_login"
               type="button"
@@ -84,8 +95,8 @@ const App = () => {
       )}
 
       {accessToken && (
-        <div className="phem-container">
-          <div className="phem-card">
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', margin: '50px 30px' }}>
+          <div style={{ color: '#024430 !important', textAlign: 'center', backgroundColor: '#fff', padding: '30px', borderRadius: '0.5rem', boxShadow: '0 1px 3px rgba(17, 24, 39, .09)', width: '100%', maxWidth: '420px', margin: '0 auto', fontFamily: 'sans-serif, serif, system-ui, -apple-system, BlinkMacSystemFont, \'Segoe UI\', Roboto, Oxygen, Ubuntu, Cantarell, \'Open Sans\', \'Helvetica Neue\', sans-serif', lineHeight: '28px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', }}>
             <img className="phe-login-img" width="250px" src="https://storage.googleapis.com/prod-phoneemail-prof-images/phem-widgets/phe-signin-success.svg" alt="phone email login demo" />
             <div className="phem-card-body">
               <h1>Welcome!</h1>
@@ -93,7 +104,7 @@ const App = () => {
                 {userDetails.countryCode} {userDetails.phoneNo}
               </h4>
             </div>
-            <button className="phem-back-button" onClick={() => window.location.href = '/'}>Back</button>
+            <button style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '14px 20px', backgroundColor: '#02BD7E', fontWeight: 'bold', color: '#ffffff', border: 'none', borderRadius: '3px', fontSize: 'inherit', cursor: 'pointer', maxWidth: '320px', width: '100%', }} onClick={() => window.location.href = '/'}>Back</button>
           </div>
         </div>
       )}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './login.css';
 import Login from './Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
